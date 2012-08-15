32d7eb1451b554ac9ff3a5a91514d1b2afdeac74
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2012-08-15 17:24:39 +0700

    quote all names in mysql queries

--- schemup/dbs/mysql.py
+++ schemup/dbs/mysql.py
@@ -1,3 +1,4 @@
+# TODO: Base class for this & PostgresSchema
 class MysqlSchema(object):
 
     def __init__(self, conn, dryRun=False):
@@ -18,7 +19,7 @@ class MysqlSchema(object):
         if log:
             # FIX: possible unicode problems and other
             if args:
-                self.runLog.append("%s [%s]" % (query, ",".join(map(str, args))))
+                self.runLog.append("%s [%s]\n" % (query, ", ".join(map(str, args))))
             else:
                 self.runLog.append(query)
 
@@ -46,15 +47,15 @@ class MysqlSchema(object):
         cur = self.conn.cursor()
         cur.execute(
             "SELECT COUNT(*)"
-            " FROM information_schema.tables"
-            " WHERE table_name = 'schemup_tables'")
+            " FROM `information_schema`.`tables`"
+            " WHERE `table_name` = 'schemup_tables'")
 
         if cur.fetchone()[0]:
             return
 
         print "Creating schema table..."
         cur.execute(
-            "CREATE TABLE schemup_tables ("
+            "CREATE TABLE `schemup_tables` ("
             " `table_name` VARCHAR(255) NOT NULL,"
             " `version` VARCHAR(255) NOT NULL,"
             " `is_current` TINYINT(1) NOT NULL DEFAULT 0,"
@@ -63,15 +64,15 @@ class MysqlSchema(object):
         self.conn.commit()
 
     def clearSchemaTable(self):
-        self.execute("DELETE FROM schemup_tables")
+        self.execute("DELETE FROM `schemup_tables`")
 
     def getSchema(self, tableName):
         cur = self.conn.cursor()
         cur.execute(
-            "SELECT column_name, data_type, is_nullable, column_default"
-            " FROM information_schema.columns"
-            " WHERE table_name = %s"
-            " ORDER BY column_name",
+            "SELECT `column_name`, `data_type`, `is_nullable`, `column_default`"
+            " FROM `information_schema`.`columns`"
+            " WHERE `table_name` = %s"
+            " ORDER BY `column_name`",
             (tableName,))
 
         return u"\n".join(u"|".join(unicode(c) for c in row) for row in cur)
@@ -79,18 +80,18 @@ class MysqlSchema(object):
     def getTableVersions(self):
         cur = self.conn.cursor()
         cur.execute(
-            "SELECT table_name, version"
-            " FROM schemup_tables"
-            " WHERE is_current = 1")
+            "SELECT `table_name`, `version`"
+            " FROM `schemup_tables`"
+            " WHERE `is_current` = 1")
         return cur
 
 
     def getVersionedTableSchemas(self):
         cur = self.conn.cursor()
         cur.execute(
-            "SELECT table_name, schema"
-            " FROM schemup_tables"
-            " WHERE is_current = 1")
+            "SELECT `table_name`, `schema`"
+            " FROM `schemup_tables`"
+            " WHERE `is_current` = 1")
         return cur
 
 
@@ -100,12 +101,12 @@ class MysqlSchema(object):
 
         cur = self.conn.cursor()
         self.execute(
-            "UPDATE schemup_tables"
-            " SET is_current = 0"
-            " WHERE table_name = %s",
+            "UPDATE `schemup_tables`"
+            " SET `is_current` = 0"
+            " WHERE `table_name` = %s",
             (tableName,), cur, log)
         self.execute(
-            "INSERT INTO schemup_tables"
+            "INSERT INTO `schemup_tables`"
             " (`table_name`, `version`, `is_current`, `schema`)"
             " VALUES (%s, %s, 1, %s)",
             (tableName, version, schema), cur, log)
@@ -116,8 +117,8 @@ class MysqlSchema(object):
 
         cur = self.conn.cursor()
         cur.execute(
-            "SELECT table_name, version"
-            " FROM schemup_tables"
-            " WHERE is_current = 1")
+            "SELECT `table_name`, `version`"
+            " FROM `schemup_tables`"
+            " WHERE `is_current` = 1")
 
         return sorted(cur.fetchall())
