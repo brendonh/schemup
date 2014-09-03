af3a0b6e9636360be06e6438bcb01f1aaeeb3aa6
Brendon Hogger <brendonh@gmail.com>
2014-09-04 00:10:14 +0800

    Bump to 1.0, because why not

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name="schemup",
-      version="0.1",
+      version="1.0",
 
       description="Database-agnostic schema upgrade tools",
       author="Brendon Hogger",
-      author_email="brendonh@cogini.com",
+      author_email="brendonh@gmail.com",
       url="https://github.com/brendonh/schemup",
       long_description=open('README.md').read(),
 
-      download_url="https://github.com/brendonh/schemup/zipball/master",
-
       packages = find_packages(),
-      zip_safe = False,
 
       install_requires = [
       ],
